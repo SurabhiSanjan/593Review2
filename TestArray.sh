0049b62da878459928c1d1db3836027ddd64af3b
SurabhiSanjan <surabhisanjan@gmail.com>
2022-09-26 20:10:00 +0530

    [SurabhiSanjan]-Review2Problems

---
+++ TestArray.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+array1=(0 1 2 3 4 5 6 7 8 9)
+
+echo ${array1[*]}
+
+echo "4th index is ${array1[3]}"
---
+++ bigger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+Var1=25;
+Var2=29;
+
+if [ $Var1 -ge $Var2 ];
+then
+echo "$Var1 is Bigger"
+
+else
+echo "$Var2 is Bigger"
+
+fi
